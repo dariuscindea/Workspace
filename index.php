af85c0cfa080d6ae0359bfc494284e05017279a2
Yousef <yousef.dev20@gmail.com>
2024-04-02 23:42:23 +0300

    First Release

--- index.php
+++ index.php
@@ -17,4 +17,6 @@ foreach ($routes as $route) {
             die(json_encode(['error' => 'Method not Allowed.']));
         }
     }
+    http_response_code(404);
+    die(json_encode(['error' => '4O4 Not Found.']));
 }
